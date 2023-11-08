4f37aca02de4fb0364ceb246b8e5be078124ae9f
Renato Lage <renato.lage85@gmail.com>
2023-11-07 22:08:45 -0300

    Adição do desafio rankeadas
    Adição do dos arquivos para cumprir o desafio de partidas rankeadas!

---
+++ desafio-felipao/desafio-classificacao-heroi/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Digite o nome do seu herói: ");
+string nome = Console.ReadLine();
+Console.WriteLine($"Digite a XP que o herói{nome} possui:");
+int xp = int.Parse(Console.ReadLine());
+switch (xp) {
+    case < 1000:
+        Console.WriteLine($"O herói{nome}, com {xp} XP, é um ferro.");
+    break;
+    case < 2000:
+        Console.WriteLine($"O herói{nome}, com {xp} XP, é um bronze.");
+    break;
+    case < 5000:
+        Console.WriteLine($"O herói{nome}, com {xp} XP, é um prata.");
+    break;
+    case < 7000:
+        Console.WriteLine($"O herói{nome}, com {xp} XP, é um ouro.");
+    break;
+    case < 8000:
+        Console.WriteLine($"O herói{nome}, com {xp} XP, é um platina.");
+    break;
+    case < 9000:
+        Console.WriteLine($"O herói{nome}, com {xp} XP, é um ascendente.");
+    break;
+    case < 10000:
+        Console.WriteLine($"O herói{nome}, com {xp} XP, é um imortal.");
+    break;
+    case > 10000:
+        Console.WriteLine($"O herói{nome}, com {xp} XP, é um RADIANTE.");
+    break;
+    default:
+        Console.WriteLine($"Erro! Não foi possível determinar o nível do herói {nome}!");
+    break;
+}
+
---
+++ desafio-felipao/desafio-partidas-rankeadas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program {
+    static int CalcRank(int vitorias, int derrotas) {
+    int resultado = vitorias - derrotas;
+    return resultado;
+    }
+    static void Main(string[] args) {
+            Console.WriteLine("Digite a quantidade de vitórias: ");
+    int qtdVitorias = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a quantidade de derrotas: ");
+    int qtdDerrotas = int.Parse(Console.ReadLine());
+    int rank = CalcRank(qtdVitorias, qtdDerrotas);
+    switch (rank) {
+        case < 10:
+        Console.WriteLine("Seu Rank é Ferro");
+            break;
+        case < 20:
+        Console.WriteLine("Seu Rank é Bronze");
+            break;
+        case < 50:
+        Console.WriteLine("Seu Rank é Prata");
+            break;
+        case < 80:
+        Console.WriteLine("Seu Rank é Ouro");
+            break;
+        case < 90:
+        Console.WriteLine("Seu Rank é Diamante");
+            break;
+        case < 100:
+        Console.WriteLine("Seu Rank é Lendário");
+            break;
+        case > 100:
+        Console.WriteLine("Seu Rank é IMORTAL");
+            break;
+        default:
+        Console.WriteLine("Não foi possível determinar seu rank!");
+        break;
+    }
+    }
+}
+
+
+
+
