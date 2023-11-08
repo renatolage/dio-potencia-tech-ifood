c55c543c70d79ba2caf94627e6346be73aa57441
Renato Lage <renato.lage85@gmail.com>
2023-11-07 23:18:12 -0300

    Adição do desafio de classes
    Adição dos arquivos para cumprir o desafio proposto sobre classes de heróis.

---
+++ desafio-felipao/desafio-classes-heroi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Heroi
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Tipo { get; set; }
+
+    public void Atacar()
+    {
+        string ataque = "";
+
+        switch (Tipo)
+        {
+            case "mago":
+                ataque = "usou magia";
+                break;
+            case "guerreiro":
+                ataque = "usou espada";
+                break;
+            case "monge":
+                ataque = "usou artes marciais";
+                break;
+            case "ninja":
+                ataque = "usou shuriken";
+                break;
+        }
+
+        Console.WriteLine($"O {Tipo} atacou usando {ataque}");
+    }
+}
